0083c104a9af085d2ccfebc34bb42c84b2391c3f
edward93 <edward.chopuryan@gmail.com>
2023-02-02 19:20:52 -0500

    adds some styles

--- src/components/Game.Component.tsx
+++ src/components/Game.Component.tsx
@@ -5,25 +5,45 @@ import "../styles/game.scss";
 
 const socket = io(`${process.env.REACT_APP_SOCKET_URL}`);
 
-type GameState = "UsernameCreated" | "Created" | "Active" | "Suspended" | "Finished";
+// type GameState = "UsernameCreated" | "Created" | "Active" | "Suspended" | "Finished";
+enum SessionState {
+  INITIATED = "INITIATED",
+  CONNECTED = "CONNECTED",
+  USERNAME_CREATED = "USERNAME_CREATED",
+  // USERNAME_CHANGED = "USERNAME_CHANGED",
+  JOINED_GAME = "JOINED_GAME",
+  LEFT_GAME = "LEFT_GAME",
+}
+
+// type Game = {
+//   currentUser: {};
+//   gameId?: string;
+//   gameState: GameState;
+//   creator: string;
+//   members?: string[];
+// };
+/**
+ * 
+ */
+type GameQuestion = {
+  id: number;
+  question: string;
+  answer?: string;
+  answeredBy?: string;
+}
 
 type Game = {
-  currentUser: {};
-  gameId?: string;
-  gameState: GameState;
-  creator: string;
-  members?: string[];
-};
+  userId: string;
+  questions: GameQuestion[];
+}
 
-let game: Game;
+
 
 const GameComponent = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [username, setUsername] = useState<string | undefined>(undefined);
-  const [gameSessionId, setGameSessionId] = useState<string | undefined>(undefined);
-
-  // const [game, setGame] = useState<Game | null>(null);
-  const [activeGame, setActiveGame] = useState(false);
+  const [username, setUsername] = useState<string>("");
+  const [gameId, setGameId] = useState<string>("");
+  const [session, setSession] = useState<{ [key: string]: any }>({ State: SessionState.INITIATED });
 
   useEffect(() => {
     socket.on("connect", () => {
@@ -34,19 +54,34 @@ const GameComponent = () => {
       setIsConnected(false);
     });
 
-    socket.on("game-created", (gameSessionId: string) => {
-      setGameSessionId(gameSessionId);
-    });
-
-    //
+    // new user joined
     socket.on("new user", (username: string) => {
       console.log(`New user has joined: '${username}'`);
+    });
+
+    // user created a new game
+    socket.on("game created", (game: any) => {
+      setGameId(game.id);
+
+      // update the main session with the current game id
+      setSession((prevSession) => ({ ...prevSession, currentGame: game, State: SessionState.JOINED_GAME }));
+    });
+
+    // new user joined
+    socket.on("joined game", async (data: any) => {
+      // log
+      console.log(`New user: ${data.userId} has joined this game: `, data.game);
+
+      // update the current session
+      setSession((prevSession) => ({ ...prevSession, currentGame: data.game, State: SessionState.JOINED_GAME }));
     });
 
     return () => {
       socket.off("connect");
       socket.off("disconnect");
-      socket.off("message");
+      socket.off("new user");
+      socket.off("game created");
+      socket.off("joined game");
     };
   }, []);
 
@@ -56,99 +91,112 @@ const GameComponent = () => {
     setUsername(value);
   };
 
-  const onCreateGameClick = (event: any) => {
-    // create a game
-    socket.emit("create-game", username);
-  };
-
-  const onJoinClick = (event: any) => {
-    // join the current game
-    socket.emit("join-game", gameSessionId);
-  };
-
-  const onGameSessionIdChange = (event: any) => {
-    const value = event.target.value;
-
-    setGameSessionId(value);
-  };
-
-  const onJoinAnotherGameClick = (event: any) => {
-    // join someone else's game
-    socket.emit("join-game", gameSessionId);
-  };
-
   /**
-   * Handles username submit button click events
+   * Handles username submit button click event
    * @param event Click event
    */
   const onCreateUserNameBtnClick = (event: any) => {
     // emit "new user" event
     socket.emit("new user", username);
 
-    // update Game object
-    game = {
-      currentUser: { [socket.id]: username },
-      creator: username ?? "",
-      gameState: "UsernameCreated",
-    };
+    // add user to the session obj
+    setSession({ ...session, CurrentUser: { id: socket.id, username }, State: SessionState.USERNAME_CREATED });
+  };
+
+  /**
+   * Handles new game create button click event
+   * @param event Click event
+   */
+  const onCreateGameBtnClick = (event: any) => {
+    // emit "create new game" event
+    socket.emit("create new game");
+  };
+
+  /**
+   * Handles join game button click event
+   * @param event Click event
+   */
+  const onJoinGameBtnClick = (event: any) => {
+    // emit join game event
+    socket.emit("join game", gameId);
+  };
+
+  /**
+   * Handles game id input change event
+   * @param event Input event
+   */
+  const onGameIdChange = (event: any) => {
+    const value = event.target.value;
+
+    setGameId(value);
   };
 
   return (
     <div className="pjw-game-container">
-      <section className="pjw-game-status-bar">{isConnected ? "connected to the server" : "...connecting"}</section>
+      <section className="pjw-game-status-bar">
+        Connection Status: {isConnected ? "connected to the server" : "...connecting"}
+      </section>
       <section className="pjw-game-main-area">
-        <section className="pjw-game-left-sidebar">Players</section>
+        <section className="pjw-game-left-sidebar">
+          {/* <div className="close">
+            <button>Close</button>
+          </div> */}
+          {(session.State === SessionState.USERNAME_CREATED || session.State === SessionState.JOINED_GAME) && (
+            <div className="pjw-game-players">
+              <div className="pjw-game-current-player">Me: {session?.CurrentUser?.username}</div>
+              <div className="pjw-game-other-players">Players ({session?.currentGame?.playersIds?.length ?? 0})</div>
+            </div>
+          )}
+        </section>
         <section className="pjw-game-mid-area">
-          <div className="pjw-game-user-creation">
-            <div className="pjw-game-username">
-              <input
-                className="pjw-input"
-                name="game-username"
-                type="text"
-                value={username}
-                placeholder="Create a username"
-                onChange={onUsernameChange}
-              />
-              <div className="pjw-input-bar" />
-            </div>
-            <button
-              className="pjw-create-username-btn"
-              onClick={onCreateUserNameBtnClick}
-              // disabled={username === undefined || username.length === 0}
-              disabled={!username}
-            >
-              Submit
-            </button>
-          </div>
-          {/* <section className="pjw-information-section">
-            <p>{gameSessionId ? `New game with '${gameSessionId}' was created` : "... No games, yet"}</p>
-          </section>
-          <section className="pjw-user-creation">
-            <div className="pjw-user-form">
-              <input type="text" value={username} placeholder="Your Username" onChange={onUsernameChange} />
-              <button className="pjw-create-game" onClick={onCreateGameClick}>
-                Create a Game
+          {session.State === SessionState.INITIATED && (
+            <div className="pjw-game-user-creation">
+              <div className="pjw-game-username">
+                <input
+                  className="pjw-input"
+                  name="game-username"
+                  type="text"
+                  value={username}
+                  placeholder="Create a username"
+                  onChange={onUsernameChange}
+                />
+                <div className="pjw-input-bar" />
+              </div>
+              <button
+                className="pjw-create-username-btn ghost-btn"
+                onClick={onCreateUserNameBtnClick}
+                disabled={!username}
+              >
+                Submit
               </button>
-              {gameSessionId ? (
-                <button className="pjw-join-game-btn" onClick={onJoinClick}>
-                  Join
-                </button>
-              ) : (
-                <>
-                  <input
-                    type="text"
-                    placeholder="Game Session Id"
-                    value={gameSessionId}
-                    onChange={onGameSessionIdChange}
-                  />
-                  <button className="pjw-join-another-game-btn" onClick={onJoinAnotherGameClick}>
-                    Join Another Game
-                  </button>
-                </>
-              )}
             </div>
-          </section>
-          <div>Game Session</div> */}
+          )}
+          {session.State === SessionState.USERNAME_CREATED && (
+            <div className="pjw-game-create-or-join-game">
+              <div className="pjw-game-join-game">
+                <div className="pjw-game-id">
+                  <input
+                    className="pjw-input"
+                    name="game-id"
+                    value={gameId}
+                    placeholder="Enter game Id"
+                    onChange={onGameIdChange}
+                    type="text"
+                  />
+                </div>
+                <button className="pjw-join-game-btn ghost-btn" disabled={!gameId} onClick={onJoinGameBtnClick}>
+                  Join Game
+                </button>
+              </div>
+              <p>Or Create</p>
+              <div className="pjw-game-create-game">
+                <button className="pjw-create-game-btn ghost-btn" onClick={onCreateGameBtnClick}>
+                  New Game
+                </button>
+              </div>
+            </div>
+          )}
+          {session.State === SessionState.JOINED_GAME && <div className="pjw-game-level-container"></div>}
         </section>
       </section>
     </div>
--- src/components/layout/Footer.Component.tsx
+++ src/components/layout/Footer.Component.tsx
@@ -7,7 +7,7 @@ const FooterComponent = () => {
         <div className="footer-app-author">Designed and Made by Ed</div>
         <div className="footer-app-version">version {process.env.REACT_APP_VERSION}</div>
         <div className="footer-app-version">
-          &copy; {new Date().getFullYear() === 2022 ? "2023" : `2023 - ${new Date().getFullYear()}`}
+          &copy; {new Date().getFullYear() === 2023 ? "2023" : `2023 - ${new Date().getFullYear()}`}
         </div>
       </div>
     </div>
